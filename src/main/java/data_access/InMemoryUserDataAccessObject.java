981bd974d7065fa85de1ff988ea03936883d96e8
Icedlemonloaf <yy.ko@mail.utoronto.ca>
2024-10-21 20:48:49 -0400

    dd

--- src/main/java/data_access/InMemoryUserDataAccessObject.java
+++ src/main/java/data_access/InMemoryUserDataAccessObject.java
@@ -41,4 +41,12 @@ public class InMemoryUserDataAccessObject implements SignupUserDataAccessInterfa
         users.put(user.getName(), user);
     }
 
+    public String getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(String currentUser) {
+        this.currentUser = currentUser;
+    }
+
 }
